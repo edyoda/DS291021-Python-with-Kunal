01ad7db77211f9068e5efc0d3fa13e1c9d555338
kunalp78 <kunalp78@gmail.com>
2021-11-22 21:48:48 +0530

    W03D01

---
+++ check.py
@@ -0,0 +1,17 @@
+a = [1,2,3]
+b = [1,2,3]
+print(id(a))
+print(id(b))
+a = (1,2,3)
+b = (1,2,3)
+print(id(a))
+print(id(b))
+
+l = [1,2,3]
+b = [1,2,3]
+
+g = (1,2,3)
+h = (1,2,3)
+
+print(g is h)
+print(l is b)
