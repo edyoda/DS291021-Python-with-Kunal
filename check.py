c971d18e8c884a64149900041dfab48d51264344
kunalp78 <kunalp78@gmail.com>
2021-12-03 17:44:43 +0530

    W04D04

--- check.py
+++ check.py
@@ -1,17 +1,19 @@
-a = [1,2,3]
-b = [1,2,3]
-print(id(a))
-print(id(b))
-a = (1,2,3)
-b = (1,2,3)
-print(id(a))
-print(id(b))
+#a = [1,2,3]
+#b = [1,2,3]
+#print(id(a))
+#print(id(b))
+#a = (1,2,3)
+#b = (1,2,3)
+#print(id(a))
+#print(id(b))
 
-l = [1,2,3]
-b = [1,2,3]
+#l = [1,2,3]
+#b = [1,2,3]
 
-g = (1,2,3)
-h = (1,2,3)
+#g = (1,2,3)
+#h = (1,2,3)
 
-print(g is h)
-print(l is b)
+#print(g is h)
+#print(l is b)
+"Vandita"
+print("Vandita 1")
